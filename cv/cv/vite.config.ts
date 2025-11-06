57e8cbba4208a48bd52e1eaf1decbe8aa1019120
Mạnh Mít <Ndmanhh@gmail.com>
2025-11-06 18:05:02 +0700

    a

--- cv/cv/vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  base: "/cv/",           // ✅ BẮT BUỘC cho GitHub Pages thư mục con
-  plugins: [react()],
-})
